6e61cb707fd5706b7e0f3e1ebdb04abf69968846
Jeelen Mavani <jmmavani47@gmail.com>
2024-12-18 18:56:16 +0530

    chanr : 0

---
+++ Patient/patient/Patients.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Xml.Linq;
+namespace hospital_management.Patient
+{
+    public partial class Patients : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //protected void btnSubmit_Click(object sender, EventArgs e)
+        //{    
+          
+        //}
+
+        //protected void btnReset_Click(object sender, EventArgs e)
+        //{
+
+        //}
+    }
+}
